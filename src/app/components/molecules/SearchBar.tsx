1ce516de783cb1932d672d9d551c9f5aa89993d7
yantiny <yantini02721@gmail.com>
2025-04-16 23:27:54 +0800

    Remove unused SearchInput component and update import path in SearchBar

--- src/app/components/molecules/SearchBar.tsx
+++ src/app/components/molecules/SearchBar.tsx
@@ -1,11 +1,11 @@
-import SearchInput from "../atoms/InputSearch";
+import SearchInput from "../atoms/SearchInput";
 import SearchButton from "../atoms/ButtonSearch";
 
 export default function SearchBar() {
   return (
     <div className="mt-6 flex justify-center">
       <div className="relative w-full max-w-md">
-        <SearchInput placeholder="Search furniture" />
+        <SearchInput />
         <SearchButton />
       </div>
     </div>
